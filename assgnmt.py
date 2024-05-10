069186f31fdf3fff5ad1cc6276aa565852b75782
JayReddy03 <100127257+JayReddy03@users.noreply.github.com>
2024-05-10 20:16:51 +0530

    Update assgnmt.py

--- assgnmt.py
+++ assgnmt.py
@@ -1,6 +1,6 @@
-print("Hello, I am Jay. This program is going to count to 10 and then something bad is going to happen!!!!!!")
+print("Hello, I am Jay. This program is going to count to 5 and then something bad is going to happen!!!!!!")
 
-for i in range(1,11):
+for i in range(1,6):
     print(i)
 
 print("Ok I lied, nothing is going to happen.")
