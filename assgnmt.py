c9f74d29d116e43a9105b2aec894caf03e6344d8
JayReddy03 <100127257+JayReddy03@users.noreply.github.com>
2024-05-10 20:07:36 +0530

    assgnmt.py

---
+++ assgnmt.py
@@ -0,0 +1,7 @@
+print("Hello, I am Jay. This program is going to count to 10 and then something bad is going to happen!!!!!!")
+
+for i in range(1,11):
+    print(i)
+
+print("Ok I lied, nothing is going to happen.")
+print("Have a good day.")
